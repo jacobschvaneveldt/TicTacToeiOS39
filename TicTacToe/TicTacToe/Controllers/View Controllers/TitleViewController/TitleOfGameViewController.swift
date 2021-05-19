99732c9d4a08d048189da878c74091a70b99cd92
SEMISI TAUFA <semisitau4@SEMISIs-MBP.localdomain>
2021-05-19 15:31:29 -0600

    Made changes to storyboard and titleOfGameVC

---
+++ TicTacToe/TicTacToe/Controllers/View Controllers/TitleViewController/TitleOfGameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TitleOfGameViewController.swift
+//  TicTacToe
+//
+//  Created by SEMISI TAUFA on 5/19/21.
+//
+
+import UIKit
+
+class TitleOfGameViewController: UIViewController {
+
+    //MARK: - OUTLETS
+    @IBOutlet weak var ticTitleLabel: UILabel!
+    @IBOutlet weak var tacTitleLabel: UILabel!
+    @IBOutlet weak var toeTitleLabel: UILabel!
+    
+    //MARK: - ACTIONS
+    @IBAction func playGameButtonTapped(_ sender: Any) {
+        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
